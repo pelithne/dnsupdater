b3ec8e6db9bbaa09c73b71eb458b25364a78f4a5
Peter <45140408+pelithne@users.noreply.github.com>
2019-10-21 14:24:50 +0200

    Update dnsupdater.py

--- dnsupdater.py
+++ dnsupdater.py
@@ -8,16 +8,8 @@ import socket
 app = Flask(__name__)
 api = Api(app)
 
-#{
-#  "appId": "9307c405-b0d3-40d3-837f-5b97a506e028",
-#  "displayName": "dnsupdatersp",
-#  "name": "http://dnsupdatersp",
-#  "password": "619b1652-368c-4300-aa09-cf07b4307c81",
-#  "tenant": "72f988bf-86f1-41af-91ab-2d7cd011db47"
-#}
-
-dnszone = "dns.ptspoc"
-resourcegroup = "PTS-PoC"
+dnszone = "private.p2spoc"
+resourcegroup = "p2spoc"
 
 class Host(Resource):
     def post(self, host):
@@ -28,14 +20,6 @@ class Host(Resource):
         args = parser.parse_args()
 
         req = 'az network private-dns record-set a add-record -g ' +resourcegroup +' -z ' +dnszone +' -a ' +request.remote_addr +' -n ' +host
-        
-#        print ("String: " +req)
-
-#        p = subprocess.Popen(['req'])
-#        try:
-#            p.wait(30)
-#        except subp.TimeoutExpired:
-#            p.kill()
 
         os.system(req)
 
